0d3e072d34d45d98cbd8fbff1487f8b744e13058
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-12-26 11:01:54 +0530

    add a new model and register to admin panel

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -5,3 +5,19 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+
+class shopping(models.Model):
+    TYPE_OF_PRODUCT_COLORS  = [
+        ('RD', 'RED'),
+        ('WH', 'WHITE'),
+        ('GRN', 'GREEN'),
+        ('BL', 'BLUE'),
+    ]
+    product_name = models.CharField(max_length=100)
+    product_price = models.IntegerField()
+    product_image = models.ImageField(upload_to="product_images/")
+    product_description = models.TextField()
+    product_colors = models.CharField(max_length=3,choices=TYPE_OF_PRODUCT_COLORS)
+
+    def __str__(self):
+        return self.product_name
